1027c190cd84409bd266e2113ae6e93608dbceaa
ngechu <ngechu08@gmail.com>
2024-02-02 18:58:15 +0300

    setup tests

---
+++ src/__tests__/Home.test.jsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import Home from "../pages/Home/Home";
+
+
+
+describe("Home", () => {
+	test("check if home is rendering correct heading", () => {
+		render(<Home />)
+		expect(screen.getByText("This is Home")).toBeDefined()
+	})
+
+	test("test click button", () => {
+		render(<Home />)
+		let count = screen.getAllByTestId("count")
+		const button = screen.getByTestId("clickButton")
+		fireEvent.click(button)
+		expect(count[0].textContent).toBe("Total number of clicks 1 ")
+	})
+
+	test("Home Page should have a p tag", () => {
+		render(<Home />)
+
+		const homeparagraph = screen.queryByRole("paragraph")
+
+		expect(homeparagraph).toBeDefined()
+	})
+})
